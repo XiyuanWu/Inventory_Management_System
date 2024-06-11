b98be619a32bbbc6eb15777b082d003a376196e2
Xiyuan Wu <xiyuan.24706826@gmail.com>
2024-06-11 22:20:27 +0000

    in testing

---
+++ build/googletest/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /workspaces/Inventory_Management_System/googletest
+# Build directory: /workspaces/Inventory_Management_System/build/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
---
+++ build/googletest/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /workspaces/Inventory_Management_System/googletest/googletest
+# Build directory: /workspaces/Inventory_Management_System/build/googletest/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
